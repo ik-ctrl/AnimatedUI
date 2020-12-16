4395001c087dd719d6564f035d304743a513bee7
ikondratyev <kondratyev.ie@gmail.com>
2020-12-16 20:49:24 +0300

    добавил главный контроллер для  рисования линий

---
+++ AnimatedUI/AnimatedUI/Source/UserControls/LineMenuUserControl.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimatedUI.Source.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для LineMenuUserControl.xaml
+    /// </summary>
+    public partial class LineMenuUserControl : UserControl
+    {
+        public LineMenuUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void StopAnimation_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void StartAnimation_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ModeChange_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Speed_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void TextBox_Delay_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
